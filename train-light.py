82d2b97690bae463acdd47a53f0d288c71b95b52
Reza Fuad Rachmadi <16111176+rezafuad@users.noreply.github.com>
2020-10-29 16:23:33 +0700

    Update train-light.py

--- train-light.py
+++ train-light.py
@@ -89,7 +89,7 @@ transform_train_list = [
         transforms.Pad(2),
         #transforms.RandomCrop((256,128)),
         transforms.RandomCrop((64,32)),
-        transforms.RandomHorizontalFlip(),
+        #transforms.RandomHorizontalFlip(),
         transforms.ToTensor(),
         transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
         ]
